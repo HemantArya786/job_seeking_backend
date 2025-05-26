113332870bf130978b2dfa4d84e2d892c993d9e1
Hemant Arya <hemantarya7860@gmail.com>
2025-05-26 19:21:00 +0530

    changes

--- api/index.js
+++ api/index.js
@@ -1,22 +1,17 @@
-console.log("API server is starting...");
-
 const express = require("express");
 const dotenv = require("dotenv");
-const jobRoutes = require("./jobs");
+const jobRoutes = require("./jobs"); // ✅ CORRECT PATH
 const serverless = require("serverless-http");
-var cors = require("cors");
+const cors = require("cors");
+
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use("/api/jobs", jobRoutes);
 
-const PORT = process.env.PORT || 3000;
-if (require.main === module) {
-  app.listen(3000, () =>
-    console.log("Server running on http://localhost:3000")
-  );
-}
+console.log("API server is starting...");
+
+app.use("/api/jobs", jobRoutes); // ✅ Matches the route
 
 module.exports.handler = serverless(app);
